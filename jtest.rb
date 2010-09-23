b29e9b60a51514fd148bfc66e57702329932bd02
rogerdpack <rogerpack2005@gmail.com>
2010-09-23 17:27:28 -0600

    add text--liking it!

--- jtest.rb
+++ jtest.rb
@@ -4,11 +4,15 @@ def dbg
   require 'ruby-debug'
   debugger
 end
+alias _dbg dbg
 
 module M
 include_package "javax.swing"
+import "java.awt.image.BufferedImage" # odd
 include_package "javax.awt"
 include_package "javax.net"
+import "java.awt.Font"
+import "java.awt.Color"
 
   class ShowImage < JFrame
     include java.awt.event.ActionListener
@@ -16,6 +20,23 @@ include_package "javax.net"
     def initialize
       @img= java.awt.Toolkit.getDefaultToolkit().getImage("c:/dev/ruby/johnpack1.jpg")      
       @timer = nil
+    end
+
+    Stats = ["Born 1809", "New Brunswick, Canada", "Your Grandfather"]
+
+    def get_image
+      image = BufferedImage.new(1000, 300, BufferedImage::TYPE_INT_RGB);
+      g = image.createGraphics()
+      # by default it's all black...
+      g.setColor( Color::WHITE )
+      g.fillRect(0,0,1000,300)
+      raise unless g.drawImage(@img, 0, 0, @img.width, @img.height, nil)
+      # now the text
+      g.setColor( Color::BLACK )
+      g.setFont(Font.new("Lucida Bright", Font::ITALIC, 60))
+      g.drawString(Stats[Time.now.to_i % Stats.length], 250, 100)
+      g.dispose
+      image
     end
     
     def paint(g)
@@ -28,9 +49,7 @@ include_package "javax.net"
       ratio*new_height = height - (Time.now.to_f*50) % height      
       g.translate(new_width, new_height)
       g.rotate(0.3, 0, 0)
-      #dbg
-      g.drawString("this is totally awesome", 20, 20)
-      g.drawImage(@img,0,0,self)
+      g.drawImage(get_image,0,0,self)
       unless @timer
         duration = 0.02*1000
         @timer = javax.swing.Timer.new(duration, self)
