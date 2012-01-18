ede89aebca50213914e8ab78df6c69e3f1bb4b02
rogerdpack <rogerpack2005@gmail.com>
2012-01-18 10:14:51 -0700

    one pass

--- lib/gedcom_parser.rb
+++ lib/gedcom_parser.rb
@@ -1,2 +1,26 @@
+require 'ostruct'
+
 class GedcomParser
+  
+  # from  2 SURN Pack
+  # extract "Pack"
+  def self.extract_single_element element, text
+    text =~ Regexp.new("\\d+ #{element} ([\\w ]+)")    
+    $1.strip
+  end
+       [{:name=>"Fred", :relation_level=>2, :gender=>"Male", :birth_place=>"New York City, New York, United States", :birth_year=>1845, 
+        :image_note_urls => ["http://dl.dropbox.com/u/40012820/kids.jpg"], :afn => "ABCD-1234"}]
+
+  def self.parse_string text
+    
+     text.split(/.*INDI.*/).select{|t| t =~ /INDI/}.map{|big_block| 
+       #2 SURN Pack
+       #2 GIVN Wesley Malin 
+       out= OpenStruct.new
+       big_block =~
+       surname = 
+       out.new 
+      
+     }
+  end
 end
--- spec/gedcom_parser.spec.rb
+++ spec/gedcom_parser.spec.rb
@@ -3,7 +3,22 @@ require 'sane'
 require 'rspec/autorun'
 require_relative '../lib/gedcom_parser'
 
+
+     [{:name=>"Fred", :relation_level=>2, :gender=>"Male", :birth_place=>"New York City, New York, United States", :birth_year=>1845, 
+        :image_note_urls => ["http://dl.dropbox.com/u/40012820/kids.jpg"], :afn => "ABCD-1234"}]
+
+
 describe GedcomParser do
-   it "should compile" do
+   it "should parse individuals" do
+    text = File.read('small.ged')
+    parsed = GedcomParser.parse_string text
+    first_person = parsed.first
+    first_person.name.should == "Lloyd Carlyle /Pack/ Sr"
+  end
+  
+  it "should extract single elements" do
+    require 'ruby-debug'
+    debugger
+    GedcomParser.extract_single_element("GIVN", "\n2 GIVN Wesley Malin \n").should == "Wesley Malin"
   end
 end
