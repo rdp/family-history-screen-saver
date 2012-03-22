00c5ac83162f2d5c22637c33a44c1aa50789b172
rogerdpack <rogerpack2005@gmail.com>
2012-03-22 11:01:23 -0600

    add call

--- run_family_history_screen_saver.bat
+++ run_family_history_screen_saver.bat
@@ -1,6 +1,6 @@
 @setlocal
 @rem disable any local rubyopt settings...
 @set RUBYOPT=
-@java -version|| echo "need to install java first" && pause && goto end
+@call java -version|| echo "need to install java first" && pause && goto end
 @cd family_history_screen_saver && call java -cp "./vendor/jruby-complete-1.6.6.jar" org.jruby.Main bin/screen_saver --gedcom
 :end
